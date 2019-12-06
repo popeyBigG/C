c67f0b504f6fbf7a3094322838833c9a54098d1c
Mateus Corrente <mateusbotelho0911@gmail.com>
2019-12-06 09:46:41 -0100

    Programa que imprime para o ecrã

---
+++ prog1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*Criar programa que imprima para o ecra*/
+
+int main() {
+
+    printf("total   =  100%\t\nIVA     =   18%\t\nURS     =   10%\t\n~~~~~~~~~~~~~~~\t\nLIQUIDO =   68%\n");
+
+}
