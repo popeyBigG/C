4dbc3b02f1db2eca9f0d94217e81224ef73bf91b
Mateus Corrente <mateusbotelho0911@gmail.com>
2019-12-06 09:56:43 -0100

    Programa que imprima para o ecra

--- prog1.c
+++ prog1.c
@@ -2,8 +2,9 @@
 
 /*Criar programa que imprima para o ecra*/
 
-int main() {
+ int void main() {
 
     printf("total   =  100%\t\nIVA     =   18%\t\nURS     =   10%\t\n~~~~~~~~~~~~~~~\t\nLIQUIDO =   68%\n");
 
+    return 0;
 }
